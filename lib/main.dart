39f62341b3bec870b0f8095d423858f465e4dfac
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-12-30 22:39:38 +0200

    change amount style

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,12 @@ class MyHomePage extends StatelessWidget {
                       child: Row(
                         children: <Widget>[
                           Container(
+                            margin: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 15),
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.black, width: 2)),
                             child: Text(tx.amount.toString()),
                           ),
                           Column(
