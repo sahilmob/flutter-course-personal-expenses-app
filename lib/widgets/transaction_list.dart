61f07e3f02b0c5db137ac27a3f1248e5bf28a004
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-01-01 22:42:16 +0200

    change app theme

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -21,13 +21,13 @@ class TransactionList extends StatelessWidget {
                   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                   padding: EdgeInsets.all(10),
                   decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2)),
+                      border: Border.all(color: Theme.of(context).primaryColor, width: 2)),
                   child: Text(
                     '\$${tx.amount.toStringAsFixed(2)}',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
-                      color: Colors.purple,
+                      color: Theme.of(context).primaryColor,
                     ),
                   ),
                 ),
