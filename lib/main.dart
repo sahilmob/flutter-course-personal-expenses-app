f8abe56e315ba5eeeba56484ce719b14491e9704
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-01-01 22:58:29 +0200

    add custom fonts and text style

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Personal Expenses",
       theme: ThemeData(
         primarySwatch: Colors.teal,
         accentColor: Colors.lime,
+        fontFamily: "Quicksand",
+        textTheme: ThemeData.light().textTheme.copyWith(
+              title: TextStyle(
+                  fontFamily: "OpenSans",
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18),
+            ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+                title: TextStyle(
+                    fontFamily: "OpenSans",
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+        ),
       ),
       home: MyHomePage(),
     );
@@ -61,7 +76,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter App"),
+        title: Text(
+          "Personal Expenses",
+        ),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -21,7 +21,8 @@ class TransactionList extends StatelessWidget {
                   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                   padding: EdgeInsets.all(10),
                   decoration: BoxDecoration(
-                      border: Border.all(color: Theme.of(context).primaryColor, width: 2)),
+                      border: Border.all(
+                          color: Theme.of(context).primaryColor, width: 2)),
                   child: Text(
                     '\$${tx.amount.toStringAsFixed(2)}',
                     style: TextStyle(
@@ -34,11 +35,10 @@ class TransactionList extends StatelessWidget {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(tx.title,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        )),
+                    Text(
+                      tx.title,
+                      style: Theme.of(context).textTheme.title,
+                    ),
                     Text(
                       DateFormat.yMMMd().format(tx.date),
                       style: TextStyle(color: Colors.grey),
